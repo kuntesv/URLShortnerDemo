21d3058d1841cf9ede35f7caf6aeeb5b489ab41b
sachin.kunte <sachin.kunte@siemens.com>
2025-02-15 09:29:34 +0530

    added changes

--- URLShortner/Program.cs
+++ URLShortner/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using URLShortner.Data;
+using URLShortner.Service;
+
 namespace URLShortner
 {
     public class Program
@@ -8,6 +12,11 @@ namespace URLShortner
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            // Add services to the container.
+            builder.Services.AddDbContext<UrlShortenerContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // Replace "DefaultConnection" with your actual connection string name
+
+            builder.Services.AddScoped<IUrlShortenerService, UrlShortenerService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
